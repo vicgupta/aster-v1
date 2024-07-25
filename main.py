2f6a2f84ecf9b1b1726f328ca251b0b07cd85b2c
Vic Gupta <vicgupta@gmail.com>
2024-07-25 08:07:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-from aster.models import OllamaChat
-from aster.agents import Agent
-
-llm = OllamaChat(model="llama3")
-pirate_agent = Agent(llm, custom_system_prompt="You are a pirate!")
-print(pirate_agent.ask(prompt="Who are you?"))
+print ("This is a test")
